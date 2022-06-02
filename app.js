eccde121036dee19d17cc412f1c974343abc8bc2
Muhammed Ensar Canbay <mecanbay@pm.me>
2022-06-02 19:38:15 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//MIDDLEWARES
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.status(201).send('<h1>Index<h2>');
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name : "about"
+  });
 });
 
 const port = 3000;

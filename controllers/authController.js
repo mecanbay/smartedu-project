0768338e6b9691be12cb4f2d9d3bd369cd53709c
Muhammed Ensar Canbay <mecanbay@pm.me>
2022-06-06 23:43:46 +0300

    Lesson 09

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
+const { use } = require("bcrypt/promises");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
@@ -6,6 +8,27 @@ exports.createUser = async (req, res) => {
     res.status(201).json({
       status: "success",
       user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = (req, res) => {
+  try {
+    const { username, password } = req.body;
+    User.findOne({ username }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            //SESSION
+            res.status(200).send("YOU ARE LOGGED IN");
+          }
+        });
+      }
     });
   } catch (error) {
     res.status(400).json({
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.route("/register").post(authController.createUser);
+router.route("/login").post(authController.loginUser)
 
 
 

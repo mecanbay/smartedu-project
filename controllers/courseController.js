6a812c1c0c3e7cc72b4968ae68a625d5ee231eae
Muhammed Ensar Canbay <mecanbay@pm.me>
2022-06-06 20:29:46 +0300

    Lesson 07

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,4 +1,6 @@
 const Course = require("../models/Course");
+const Category = require("../models/Category");
+
 
 exports.createCourse = async (req, res) => {
   try {
@@ -18,9 +20,23 @@ exports.createCourse = async (req, res) => {
 
 exports.viewCourses = async (req, res) => {
   try {
-    const courses = await Course.find();
+    let filter = {}
+    if(req.query.categories){
+      const category = await Category.findOne({slug : req.query.categories})
+      filter = {category:category._id}
+    }
+    
+    // const categorySlug = req.query.categories;
+
+    // if(categorySlug){
+    //   const category = Category.findOne({slug : categorySlug});
+    // }
+    const courses = await Course.find(filter);
+    const categories = await Category.find();
+
     res.render("courses", {
       courses,
+      categories,
       page_name: "courses",
     });
   } catch (error) {
@@ -34,6 +50,7 @@ exports.viewCourses = async (req, res) => {
 exports.viewCourse = async (req, res) => {
   try {
     const course = await Course.findOne({slug : req.params.slug});
+    const categories = await Category.find();
 
     // res.status(200).json({
     //   status : "success",
@@ -42,6 +59,7 @@ exports.viewCourse = async (req, res) => {
 
     res.render("course", {
       course,
+      categories,
       page_name: "courses",
     });
   } catch (error) {
